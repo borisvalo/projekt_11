3ec2cfe8534bc37cac7ea8b071aa7119aa6d7297
paulslaby <paulie@ubuntu.(none)>
2011-10-03 15:44:42 +0200

    Naznačena souborová struktura programu

---
+++ ial.c
@@ -0,0 +1,17 @@
+/*
+ * Soubor obsahující funkce do předmětu IAL
+ * Autor: Pavel Slabý, xslaby00
+ * 				a další
+ * Datum odevzdání: 11.12.2011
+ */
+
+
+/*
+ * Funkce provádějící řazení pomocí algoritmu HEAP SORT
+ * @param 
+ * @return chybový kód
+ */
+int sort (){
+
+
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * Soubor obsahující funkce do předmětu IAL
+ * Autor: Pavel Slabý, xslaby00
+ * 				a další
+ * Datum odevzdání: 11.12.2011
+ * Použití: TODO: dopsat
+ */
+
+int main(){
+
+
+ return 0;
+}
