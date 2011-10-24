2c25f093b06ecb5750eba80bcc33d477204909e0
paulslaby <paulie@ubuntu.(none)>
2011-10-24 21:53:28 +0200

    Druhý pokus o přidání main.c, není to vše, ale jdu spát :-D

--- main.c
+++ main.c
@@ -1,13 +1,50 @@
+#include <stdio.h>
+#include "main.h"
 /*
- * Soubor obsahující funkce do předmětu IAL
  * Autor: Pavel Slabý, xslaby00
  * 				a další
  * Datum odevzdání: 11.12.2011
- * Použití: TODO: dopsat
  */
+ 
+#define ERR_OK 0
+#define ERR_LEX 1
+#define ERR_SYNTAX 2
+#define ERR_SEMANT 3
+#define ERR_INTERPRET 4
+#define ERR_INTERNI 5
 
-int main(){
 
-
- return 0;
+int main(int argc, char** argv)){
+	FILE *soubor;
+	if (argc == 1)
+	{
+		printf("Nezadan argument\n");
+		return FILE_ERROR;
+	}
+	if ((f = fopen(argv[1], "r")) == NULL)
+	{
+		printf("Nepodarilo se otevrit soubor\n");
+		return FILE_ERROR;
+	}
+	
+	tTabulkaSymbolu TabulkaSymbolu;
+	tabInit(&TabulkaSymbolu);
+	
+	tVnitrni vnitrni;//TODO:
+	
+	kodChyby = syntakticky_analyzator(&TabulkaSymbolu, &vnitrni);
+	
+	
+	
+	//(TODO: )odalokování zdrojů
+	fclose(soubor);
+	tableDispose(&TabulkaSymbolu);
+	
+	if(kodChyby!=ERR_OK){
+		return kodChyby;
+	}
+	
+	interpretuj(&vnitrni);
+	
+	return ERR_OK;
 }
