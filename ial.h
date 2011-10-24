8f8dd6639f125d26f7abcf7df2325f4f7eed87b0
paulslaby <paulie@ubuntu.(none)>
2011-10-24 21:47:28 +0200

    přidán main.c

---
+++ ial.h
@@ -0,0 +1,43 @@
+/*
+ * Soubor obsahující funkce do předmětu IAL
+ * Autor: Pavel Slabý, xslaby00
+ * 				a další
+ * Datum odevzdání: 11.12.2011
+ */
+
+
+typedef TPolozka {
+	int hodnota;
+	TPolozka *nasledujici;
+}TPolozka
+
+typedef TSeznam {
+	TPolozka *prvni;
+}
+
+
+
+/*
+ * Funkce provádějící řazení pomocí algoritmu HEAP SORT
+ * @param 
+ * @return chybový kód
+ */
+int sort (){
+
+
+}
+
+/*
+ * Funkce provádějící vyhledávání pomocí Knuth-Moris-Prattova algoritmu
+ * @param 
+ * @return chybový kód
+ */
+int sort (){
+
+
+}
+
+
+/*
+ * Tabulka symbolů
+ */
